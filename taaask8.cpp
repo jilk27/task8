ca36656320a26d62c667d6ed5311025732eef6fe
jilk27 <frolovkirill2007@yandex.ru>
2024-12-20 13:14:11 +0300

    Add files via upload

---
+++ taaask8.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <bitset>
+#include <string>
+using namespace std;
+
+string sum(const string& a, const string& b) {
+    string res;
+    int carry = 0;
+    int maxlen = max(a.length(), b.length());
+
+    string af = string(maxlen - a.length(), '0') + a;
+    string bf = string(maxlen - b.length(), '0') + b;
+
+    for (int i = maxlen - 1; i >= 0; --i) {
+        int bitA = af[i] - '0';
+        int bitB = bf[i] - '0';
+        int sum = bitA + bitB + carry;
+
+        res.insert(res.begin(), (sum % 2) + '0');
+        carry = sum / 2;
+    }
+
+    if (carry) {
+        res.insert(res.begin(), carry + '0');
+    }
+
+    return res;
+}
+
+int main() {
+    string A = "101101";
+    string B = "110011";
+
+    bitset<6> abit(A);
+    bitset<6> bbit(B);
+    bitset<6> ares = abit & bbit;
+
+    cout << "A AND B: " << ares << endl;
+
+    string summ = sum(A, B);
+
+    cout << "A + B: " << summ << endl;
+
+    return 0;
+}
